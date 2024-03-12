7e77de3e69b219d27b6e70bfdd7225ed5c446c44
Margarita_Pavlova <rito4ka298@yandex.ru>
2024-03-12 13:06:36 +0300

    all

--- index.js
+++ index.js
@@ -1,73 +1,152 @@
-const windowInnerWidth = window.innerWidth;
-const videos = document.querySelectorAll(".video__main-slider");
-const mainVideo = document.getElementById("main-video_slider").getElementsByTagName('iframe')[0];
-const onasCarousel = document.querySelector(".carousel__main-slider");
-const firstImage = onasCarousel.querySelectorAll("img")[0];
-const arrowIcons = document.querySelectorAll(".arrow__main-slider");
 
-const imageHeight = 180 + 19;
-const imageWidth = 320 + 15;
+  //
 
-const scrollHeight = onasCarousel.scrollHeight - onasCarousel.clientHeight;
-const scrollWidth = onasCarousel.scrollWidth - onasCarousel.clientWidth;
+  class Popup {
+    constructor(popupSelector) {
+      this._popupSelector = document.querySelector(popupSelector);
+      this._closeButton = this._popupSelector.querySelector(".popup__close");
+      this.close = this.close.bind(this);
+      this.handleEscClose = this.handleEscClose.bind(this);
+    }
 
-arrowIcons.forEach(icon => {
-	icon.addEventListener("click", () => {
-		if (windowInnerWidth > 1200) {
-			onasCarousel.scrollTop += icon.id === "up" ? -imageHeight : imageHeight;
-		} else {
-			onasCarousel.scrollLeft += icon.id === "up" ? -imageWidth : imageWidth;
-		}
-	})
-})
+    open() {
+      this._popupSelector.classList.add("popup_opened");
+      document.addEventListener("keydown", this.handleEscClose);
+    }
 
-// videos.forEach(video => {
-// 	video.addEventListener("click", () => {
-// 		const iframeVideo = video.querySelector("iframe");
-// 		if (mainVideo.src !== iframeVideo.src) mainVideo.src = iframeVideo.src;
-// 	})
-// })
+    close() {
+      this._popupSelector.classList.remove("popup_opened");
 
-videos.forEach(video => {
-    video.addEventListener("click", () => {
-        const iframeVideo = video.querySelector("iframe");
-        const mainVideoSrc = mainVideo.src;
-        const mainView = document.querySelector(".views_main_slider span");
-        const videoViews = video.querySelector(".views_main_slider span");
-        console.log(videoViews, mainView);
-        if (mainVideo.src !== iframeVideo.src) {
-            const iframeVideoSrc = iframeVideo.src;
-            mainVideo.src = iframeVideoSrc;
-            iframeVideo.src = mainVideoSrc;
+      if (this._popupSelector.querySelector('iframe')) {
+        this._popupSelector.querySelector('iframe').removeAttribute("src");
+      }
+    }
 
-            const tmp = mainView.textContent;
-            mainView.textContent = videoViews.textContent;
-            videoViews.textContent = tmp;
+    handleEscClose() {
+      if (event.key === "Escape") {
+        this.close();
+      }
+    }
+
+    setEventListeners() {
+      this._closeButton.addEventListener("click", this.close);
+
+      this._popupSelector.addEventListener("click", (event) => {
+        if (event.currentTarget === event.target) {
+          this.close();
         }
-    })
-})
+      });
+    }
+  }
 
+  const videoImage = document.querySelector(".info__video-block img");
+const popupVideo = document.querySelector(".popup_video");
 
+const videoPopup = new Popup(".popup_video");
 
-
-// mobile
-
-const mobileArrowIcons = document.querySelectorAll(".wrapper-mobile_main-slider .arrow__main-slider");
-const mobileMainVideo = document.getElementById("mobile-main-video_slider").getElementsByTagName('iframe')[0];
-let ids = Array.from(document.querySelectorAll(".video__main-slider iframe")).map(iframe => iframe.src.split("/").pop());
-let currentId = ids.indexOf(mobileMainVideo.src.split("/").pop());
-
-if (currentId === -1) {
-    ids.push(mobileMainVideo.src.split("/").pop());
+if (videoPopup) {
+  videoPopup.setEventListeners();
 }
+function openVideoPopup(video) {
+    videoPopup.open();
+    popupVideo.querySelector(".popup__video").setAttribute("src", video);
+  }
 
-mobileArrowIcons.forEach(icon => {
-    icon.addEventListener("click", () => {
-        if (icon.id === "left") {
-            currentId = (currentId - 1 + ids.length) % ids.length;
-        } else if (icon.id === "right") {
-            currentId = (currentId + 1) % ids.length;
-        }
-        mobileMainVideo.src = "https://www.youtube.com/embed/" + ids[currentId];
-    })
-})
+const popupFeedback = document.querySelector(".popup_feedback");
+  let addMore = document.querySelector(".feedbacks__more");
+
+  const feedbackList = document.querySelector(".feedbacks__list");
+  let feedbacksItems;
+  let feedbackVideos;
+  let feedbacks;
+
+  if (feedbackList) {
+    feedbacksItems = Array.from(
+      feedbackList.querySelectorAll(".feedbacks__list li")
+    );
+    
+    feedbackVideos = Array.from(feedbackList.querySelectorAll(".feedbacks__preview"));
+    feedbacks = Array.from(feedbackList.querySelectorAll(".feedback"));
+  }
+
+  const cardAmmount = 6;
+  const feedbackPopup = new Popup(".popup_feedback");
+
+  if (feedbackPopup) {
+    feedbackPopup.setEventListeners();
+  }
+
+  function openFeedbackPopup(e) {
+    console.log ('xcd');
+    feedbackPopup.open();
+
+    const container = popupFeedback.querySelector(".popup__content div");
+    const el = e.currentTarget.querySelector(".feedback div").cloneNode(true);
+
+    container.replaceWith(el);
+  }
+
+  function addMoreFeedback() {
+    // найти первый скрытый элемент
+    let lastVisibleIndex = feedbacksItems.findIndex((el) =>
+      el.classList.contains("hidden")
+    );
+
+    // открыть 6 следующих скрытых элементов
+    feedbacksItems.forEach((el, index) => {
+      if (index < lastVisibleIndex + cardAmmount) {
+        el.classList.remove("hidden");
+      }
+    });
+
+    // проверка на наличие еще элементов
+    if (lastVisibleIndex + cardAmmount === feedbacksItems.length) {
+      addMore.classList.add("hidden");
+      addMore.removeEventListener("click", addMoreFeedback);
+    }
+  }
+
+  // скрыть лишние элементы
+  if (feedbackList) {
+    feedbacksItems.forEach((el, index) => {
+
+      if (index > 5) {
+        el.classList.add("hidden");
+      }
+    });
+
+  // скрыть кнопку, если отзывов меньше 7
+    if (feedbacksItems.length <= 6) {
+      addMore.classList.add("hidden");
+    }
+
+    if (feedbackVideos) {
+      feedbackVideos.forEach((el) => {
+        el.addEventListener("click", (e) => openVideoPopup(e.target.name));
+    });
+    }
+
+    if (feedbacks) {
+      feedbacks.forEach((el) => {
+        el.addEventListener("click", openFeedbackPopup);
+       
+      });    
+    }
+  }
+
+  if (addMore) {
+    addMore.addEventListener("click", addMoreFeedback);
+  }
+
+  // построенные обьекты - отзывы 
+
+  const objectsFeedback = Array.from(document.querySelectorAll('.object-row__feedback .feedback'));
+
+  if (objectsFeedback.length >= 1 ) {
+    
+    objectsFeedback.forEach((el) => {
+      el.addEventListener("click", openFeedbackPopup);
+    });  
+  }
+
+
